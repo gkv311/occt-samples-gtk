5db8ea7b0298d67cc5ccb56bb258ec7e2dbbb58b
Kirill Gavrilov <kirill@sview.ru>
2025-12-19 19:44:51 +0300

    CI/CD - add building job for Linux

---
+++ .github/workflows/screenshot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+anApp=$1
+anImg=$2
+aDelay=$3
+
+"$anApp" &
+anAppPid=$!
+
+rm -f "$anImg"
+scrot -d $aDelay -F "$anImg"
+
+anImgSize=$(wc -c <"$anImg")
+if [ $anImgSize -lt 2000 ]; then
+  # retry one more time
+  mv -f "$anImg" "${anImg}-back-first.png"
+  scrot -d 1 -F "$anImg"
+fi
+
+kill $anAppPid
