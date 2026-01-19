dddbef0776154701dbf62ac67d7df8e85fc74d7f
Kirill Gavrilov <kirill@sview.ru>
2026-01-19 07:16:02 +0300

    Add --verbose command-line argument to enable Message_Trace messages from OCCT

--- occt-gtk4-glarea/main.cpp
+++ occt-gtk4-glarea/main.cpp
@@ -5,6 +5,7 @@
 #include "../occt-gtk-tools/OcctGtkTools.h"
 
 #include <Message.hxx>
+#include <Message.hxx>
 #include <OSD.hxx>
 #include <OSD_Environment.hxx>
 
@@ -14,6 +15,21 @@ int main(int theNbArgs, char* theArgVec[])
 {
   OSD::SetSignal(false);
   //OSD::SetSignalStackTraceLength(10);
+
+  for (int anArgIter = 0; anArgIter < theNbArgs; ++anArgIter)
+  {
+    // enable verbose messages from OCCT algorithms
+    if (std::strcmp(theArgVec[anArgIter], "-v") == 0
+     || std::strcmp(theArgVec[anArgIter], "--verbose") == 0)
+    {
+      if (anArgIter + 1 < theNbArgs)
+        theArgVec[anArgIter] = theArgVec[anArgIter + 1];
+
+      --anArgIter;
+      --theNbArgs;
+      Message::DefaultMessenger()->Printers().First()->SetTraceLevel(Message_Trace);
+    }
+  }
 
   // force X11 backend for OpenGL initialization using GLX
   // (should be done in sync with OCCT configuration)
